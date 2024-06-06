8ec1c8dbbc00f18e7b145b91ed63fb56726a25df
arunacharya1603 <arunacharya1603@gmail.com>
2024-06-06 20:55:44 +0530

    update

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Services = () => {
   return (
-    <div className='mt-20 p-8 bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg flex justify-center flex-col items-center md:items-start'>
+    <div className='mt-20 p-8 bg-white h-screen bg-opacity-10 backdrop-filter backdrop-blur-lg flex justify-center flex-col items-center md:items-start'>
       <h2 className="text-3xl font-bold mb-4 text-black">Services</h2>
       <ul className="list-disc list-inside font-mono font-semibold text-xl text-black ">
         <li>Civil Litigation</li>
